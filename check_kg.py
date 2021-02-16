63008ce81fd32815fa9d4b42762a6f24000f7b27
Raj Nath Patel <patelrajnath@gmail.com>
2021-02-16 23:06:53 +0000

    Add KG for English

--- check_kg.py
+++ check_kg.py
@@ -4,7 +4,8 @@ from brain.knowgraph_english import KnowledgeGraph
 vocab_file = "D:\Downloads\ent_vocab_custom"
 kg = KnowledgeGraph(vocab_file=vocab_file, predicate=True)
 text = "Delhi is the capital of India ."
-tokens, pos, vm, _ = kg.add_knowledge_with_vm([text], add_pad=False, max_length=16)
+tokens, pos, vm, tag = kg.add_knowledge_with_vm([text], add_pad=False, max_length=16)
+print(tag)
 print(pos)
 print(tokens)
 print(vm)
