0b1ed3212a689afa4acbb9999a7e2c7881d6aac6
Raj Nath Patel <patelrajnath@gmail.com>
2021-02-24 10:30:59 +0000

    Update evaluation code

--- train.sh
+++ train.sh
@@ -4,4 +4,5 @@ python run_kg_luke_ner.py \
     --train_path data/conll_2003/eng.train.train.csv \
     --dev_path data/conll_2003/eng.testb.dev.csv \
     --test_path data/conll_2003/eng.testa.dev.csv \
-    --kg_name D:\Downloads\ent_vocab_custom --use_kg
+    --kg_name D:\\Downloads\\ent_vocab_custom --use_kg \
+    --pretrained_model_path  D:\\Downloads\\luke_base_500k.tar.gz
