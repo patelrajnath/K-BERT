d19393d054513cca335edb4808a05c2cde79a6d3
Raj Nath Patel <patelrajnath@gmail.com>
2021-02-27 15:44:53 +0000

    Add code for creating model archive

--- run_kg_luke_ner.py
+++ run_kg_luke_ner.py
@@ -3,18 +3,49 @@
   This script provides an K-BERT example for NER.
 """
 import argparse
+import json
+import os
+import tarfile
+import tempfile
+from pathlib import Path
+
 import torch
 import torch.nn as nn
 
 from brain import config
 from brain.knowgraph_english import KnowledgeGraph
 from luke import ModelArchive, LukeModel
+from luke.utils.model_utils import METADATA_FILE, MODEL_FILE, get_entity_vocab_file_path
 from uer.utils.config import load_hyperparam
 from uer.utils.optimizers import BertAdam
 from uer.utils.constants import *
 from uer.utils.seed import set_seed
 from uer.model_saver import save_model
 from torch.nn import functional as F
+
+
+def create_model_archive(model_file: str, out_file: str, compress: str):
+    model_dir = os.path.dirname(model_file)
+    json_file = os.path.join(model_dir, METADATA_FILE)
+    with open(json_file) as f:
+        model_data = json.load(f)
+        del model_data["arguments"]
+
+    file_ext = ".tar" if not compress else ".tar." + compress
+    if not out_file.endswith(file_ext):
+        out_file = out_file + file_ext
+
+    with tarfile.open(out_file, mode="w:" + compress) as archive_file:
+        archive_file.add(model_file, arcname=MODEL_FILE)
+
+        vocab_file_path = get_entity_vocab_file_path(model_dir)
+        archive_file.add(vocab_file_path, arcname=Path(vocab_file_path).name)
+
+        with tempfile.NamedTemporaryFile(mode="w") as metadata_file:
+            json.dump(model_data, metadata_file, indent=2)
+            metadata_file.flush()
+            os.fsync(metadata_file.fileno())
+            archive_file.add(metadata_file.name, arcname=METADATA_FILE)
 
 
 def loss_fn(outputs, labels, mask):
