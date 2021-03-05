17e67768e296e9dd78d48c49515ce6a44b41ce5c
Raj Nath Patel <patelrajnath@gmail.com>
2021-03-05 11:28:39 +0000

    Add eval code

---
+++ eval_ner.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+prefix='eng'
+prefix='deuutf'
+# prefix='ned'
+# prefix='esp'
+# prefix='alliance'
+# prefix='wallet'
+# prefix='accounts'
+#  prefix='ubuntu'
+# prefix='nlu'
+# prefix='snips'
+
+postfix='_bert'
+# postfix=''
+
+python eval_ner.py --output_dir outputs \
+    --t_labels ${prefix}_label${postfix}.txt \
+    --p_label ${prefix}_predict${postfix}.txt \
+    --text ${prefix}_text${postfix}.txt
