78a624af04fd720389dbbe3c954241b263a9db6a
Raj Nath Patel <patelrajnath@gmail.com>
2021-03-31 14:54:29 +0100

    Add NEW datasets

--- data/combine_csv.py
+++ data/combine_csv.py
@@ -7,7 +7,7 @@ df_twitter = pd.read_csv('tweeter_nlp/ner.txt.train_filtered.csv', sep='\t', ind
 df_gmb = pd.read_csv('GMB/ner_filtered.csv', sep='\t', index_col=None, header=0)
 df_kaggle = pd.read_csv('kaggle-ner/ner_filtered.csv', sep='\t', index_col=None, header=0)
 
-df_combined = pd.concat([df_kaggle, df_gmb, df_twitter], axis=0, ignore_index=True)
+df_combined = pd.concat([df_gmb, df_twitter, df_kaggle], axis=0, ignore_index=True)
 print(df_combined.count(axis=0))
 df_unique = df_combined.drop_duplicates()
 df_test = df_unique
