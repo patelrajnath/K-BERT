5d5c20e8145295105f710c3a4f20933f00b01877
Raj Nath Patel <patelrajnath@gmail.com>
2021-04-15 12:17:59 +0000

    Add shell scripts.

--- evaluate_many_only_kg_voted_selection.sh
+++ evaluate_many_only_kg_voted_selection.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-for kg in /RAJ/ent_vocab_custom_cleaned_freq-sorted /RAJ/ent_vocab_custom_filtered_sorted.json;
+for kg in /RAJ/ent_vocab_custom_cleaned_freq-sorted #/RAJ/ent_vocab_custom_filtered_sorted.json;
 do
-	for n in 2 5 15 10 20 30 50;
+	for n in 15 #10 15 20 30 50;
 	do
 		echo $kg
 		echo $n
-		prefix=normalized_voting-choicer_max-ent-${n}
+		prefix=voting-choicer_max-ent-${n}
 		kg_filename="$(basename -- $kg)"
 		kg_filename="${kg_filename%%.*}"
 		echo $kg_filename
-		CUDA_VISIBLE_DEVICES=1 nohup python -u run_kg_luke_ner.py \
-		  --train_path data/conll_2003/eng.train.train_normalized.csv \
-		  --dev_path data/conll_2003/eng.testb.dev_normalized.csv \
-		  --test_path data/conll_2003/eng.testa.dev_normalized.csv \
+		CUDA_VISIBLE_DEVICES=1 nohup python -u kg_luke_ner_evaluate.py \
+		  --train_path data/conll_2003/eng.train.train_filtered.csv \
+		  --dev_path data/conll_2003/eng.testb.dev.csv \
+		  --test_path data/conll_2003/eng.testa.dev.csv \
 			--pretrained_model_path /RAJ/luke_base_500k.tar.gz \
 			--output_model_path ./outputs/kbert_conll2003_${prefix}_${kg_filename}.bin \
 			--kg_name $kg \
@@ -25,7 +25,7 @@ do
 			--max_entities ${n} \
 			--eval_range_with_types \
 			--output_file_prefix outputs/evaluation/${prefix}_${kg_filename} \
-			> ./outputs/logs-voting-choicer/kbert_conll_${prefix}_${kg_filename}.log
+			> ./outputs/logs-voting-chicer/original_test_kbert_conll_${prefix}_${kg_filename}.log
 			#--reverse_order \
 			#--use_subword_tag \
 	done
